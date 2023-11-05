60dac33647be8eb4b566cb02c4929427ee0db677
GabyAM <gdgabyam@gmail.com>
2023-11-04 23:52:46 -0300

    adds game end
    now, after every turn the program checks if the game is over, wheter by draw or win. If i'ts over, the user won't be able to input anymore and a text announcing the winner will appear.

--- index.js
+++ index.js
@@ -18,9 +18,7 @@ const displayController = (function() {
         $board.innerHTML = '';
         board.forEach((cell, index) => {
             const $cell = createCellElement(cell, index);
-            $cell.addEventListener('click', () => {
-                game.handlePlaceMark(index);
-            })
+            $cell.onclick = () => {game.handlePlaceMark(index)};
             $board.appendChild($cell);
         })
     }
@@ -44,7 +42,23 @@ const gameboard = (function() {
         console.log(board)
         displayController.displayBoard(board);
     }
-    return {board /*ONLY TO TEST*/, getValue, placeMark, resetBoard}
+
+    function isFull() {
+        return board.every(cell => cell !== null)
+    }
+    function getWinnerCombos() {
+        return [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8]
+        ];
+    }
+    return {board /*ONLY TO TEST*/, getValue, placeMark, getWinnerCombos, resetBoard, isFull}
 })()
 
 
@@ -81,22 +95,55 @@ const game = (function() {
             : playerOnTurn = player1
     }
 
+    function getWinner() {
+        for(const combo of gameboard.getWinnerCombos()) {
+            const [a, b, c] = combo;
+            if(
+                gameboard.getValue(a) &&
+                gameboard.getValue(a) === gameboard.getValue(b) && 
+                gameboard.getValue(a) === gameboard.getValue(c)
+                ) {
+                    if (gameboard.getValue(a) === player1.getSymbol()) {
+                        return player1;
+                    }
+                    return player2;
+            }
+        }
+    }
+
+    function isGameOver() {
+        return (gameboard.isFull() || getWinner() !== undefined);
+    }
+
     function handlePlaceMark(position) {
         if(gameboard.getValue(position) === null) {
             playerOnTurn.placeMark(position)
             newTurn();
+        }
+        if(isGameOver()) {
+            end();
         }
     }
 
     function end() {
         const $cells = document.querySelectorAll('.board .cell');
         $cells.forEach($cell => {
-            $cell.removeEventListener('click', () => {
-                handlePlaceMark($cell.dataset.position)
-            })
+            $cell.onclick = {};
         })
+        const $resultText = document.querySelector('.result-text'); 
+        const winner = getWinner();
+        console.log(winner)
+        if(winner === undefined) {
+            $resultText.textContent = 'Draw!'
+        }
+        else if(winner.name === player1.name) {
+            $resultText.textContent = 'Player 1 wins!'
+        } else {
+            $resultText.textContent = 'Player 2 wins!'
+        }
+        $resultText.style.visibility = 'visible';
     }
-    return {start, newTurn, handlePlaceMark, end}
+    return {start, handlePlaceMark, end}
 })()
 
 game.start();
